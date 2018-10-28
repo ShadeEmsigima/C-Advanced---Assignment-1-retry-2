b7fa6ebb6b282bead069d4209d0589c5acf8fe43
Li <36532694+ShadeEmsigima@users.noreply.github.com>
2018-10-28 09:11:18 +0100

    Completed assignment 1
    - Completed it with using I think the outputiterator (or forward iterator).  Im still a little bit confused.
    
    NEXT
    Assignment 2: show EC points per student.

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -34,52 +34,71 @@ int main()
 	std::cout << std::endl;
 	std::cout << std::endl;
 
-	//make 3 teachers on the heap, and assign them. Print this.
-	Teacher* Valentijn = new Teacher("Valentijn"); Valentijn->assignTo(Python);
+	//make 3 teachers on the heap, and assign them, add them to vector, print this.
+	std::vector<Teacher*> teachers;
+	Teacher* Valentijn = new Teacher("Valentijn"); Valentijn->assignTo(Python);  teachers.push_back(Valentijn);
 	std::cout << "Heap saved teacher " << Valentijn->name << std::endl << "Assigned to: " << Valentijn->assignedTo << std::endl;
-	Teacher* Ton = new Teacher("Ton"); Ton->assignTo(HTML);
+	Teacher* Ton = new Teacher("Ton"); Ton->assignTo(HTML); teachers.push_back(Ton);
 	std::cout << "Heap saved teacher " << Ton->name << std::endl << "Assigned to: " << Ton->assignedTo << std::endl;
-	Teacher* Edwin = new Teacher("Edwin"); Edwin->assignTo(CPP);
+	Teacher* Edwin = new Teacher("Edwin"); Edwin->assignTo(CPP); teachers.push_back(Edwin);
 	std::cout << "Heap saved teacher " << Edwin->name << std::endl << "Assigned to: " << Edwin->assignedTo << std::endl;
 	
 
 	std::cout << std::endl;
 
-	//making students on heap using vectors with pointers to students. Inspired by teacher's example code
-	std::vector<Student*> studenten;
+	std::vector<Student*> students;
 
 	//ONE WAY OF GETTING RANDOM NUM:
-	//for (int i = 0; i < 10; i++) {
-	//	//picking random pos on vector of modules to assign student to a random module
-	//	//https://stackoverflow.com/questions/5008804/generating-random-integer-from-a-range
-	//	int min = 0;
-	//	int max = 2;
-	//	int output = min + (rand() % static_cast<int>( max - min + 1));
-	//	return output as at(HERE)
-	//}
+	for (int i = 0; i < 10; i++) {
+		//picking random pos on vector of modules to assign student to a random module
+		//https://stackoverflow.com/questions/5008804/generating-random-integer-from-a-range
+		int min = 0;
+		int max = 2;
+		int output = min + (rand() % static_cast<int>( max - min + 1));
 
-	//NEXT TIME
-	//get this random stuff to work and assign student a random module in the vector loop
-	//srand(time(NULL)*2);
-	//for (int i = 0; i < 2; i++) {
-	//	thePassword += charList[rand() % 52];
-	//}
-
-	Student* nieuweStudent = new Student("Pietje Puk"); //save on heap with name
-	nieuweStudent->assignTo(modules.at()); //now assign it
+	Student* nieuweStudent = new Student("Pietje Puk");
+	nieuweStudent->assignTo(modules.at(output)); students.push_back(nieuweStudent);
 	std::cout << "Heap saved " << nieuweStudent->name << ", assigned to module " << nieuweStudent->assignedTo << std::endl;
+	}
 	
 	std::cout << std::endl;
 
 	std::cout << "ASSIGNMENT ====================================================" << std::endl;
 	
 	//-Print a list of modules, with students and teachers
+	std::vector<Module*>::iterator it;
+	std::vector<Teacher*>::iterator j = teachers.begin();
+	std::vector<Student*>::iterator k = students.begin();
 
+	for (it = modules.begin(); it < modules.end(); it++) {
+		std::cout << (*it)->name << ": " << std::endl; //print out the current module
+		for (j = teachers.begin(); j < teachers.end(); j++) {
+			if ((*j)->assignedTo == (*it)->name) { //if the module the object is assigned to is the same were looking for, print it.
+				std::cout << (*j)->name << " is assigned to " << (*j)->assignedTo << std::endl;
+			for (k = students.begin(); k < students.end(); k++) {
+				if ((*k)->assignedTo == (*it)->name) { //do the same for students
+					std::cout << (*k)->name << ", is assigned to " << (*k)->assignedTo << std::endl;
+				}
+			}
+			}
+		}
+		std::cout << std::endl;
+	}
+
+
+
+
+
+
+	//		it++;
+	//	}
+	//}
 
 	std::cout << std::endl;
 	system("PAUSE");
 }
 
+	//CODE THAT WAS NEVER USED
 	////make 10 students on the heap
 	////I tried a for loop to make new students, but failed
 	//Student* s1 = new Student("Pietje 1"); s1->assignTo(Python);
@@ -111,3 +130,9 @@ int main()
 	//
 	//Student* s10 = new Student("Pietje 10"); s10->assignTo(Python); //make student, assign student
 	//std::cout << "Heap saved student " << s10->name << std::endl << "Assigned to: "<< s10->assignedTo << std::endl;
+
+	/*int returnValue = 0;
+	srand(time(NULL)*2);
+	for (int i = 0; i < 2; i++) {
+		returnValue += 10[std::rand() % 52];
+	}*/
